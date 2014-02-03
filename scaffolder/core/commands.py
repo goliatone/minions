ad25ab1af72dabe8407c0414a6ee95548f81bc80
goliatone <burgosemi@gmail.com>
2014-02-02 22:37:22 -0500

    Commands: Refactoring, integration with CommandOptionParser.

--- scaffolder/core/commands.py
+++ scaffolder/core/commands.py
@@ -8,12 +8,9 @@ from optparse import make_option
 from scaffolder.core.utils import import_class
 from scaffolder.core.optparser import CommandMeta
 from scaffolder.core.optparser import CommandOptionParser
+from scaffolder import get_version
 
 
-COMMANDS = [
-    'template',
-    'vcs'
-]
 
 
 class CommandController():
@@ -21,27 +18,36 @@ class CommandController():
     DEFAULT_ARGUMENT = ['--help']
 
     def __init__(self, stdout=None, stderr=None):
+
         self.stdout = stdout or sys.stdout
         self.stderr = stderr or sys.stderr
+
+        #Hold a ref to all Cmd's id
+        self.command_ids = []
 
         self.prog = ''
         self.command = ''
         self.argv = ()
+
         #Make CliApplication, instantiate a new CommandController and set
         self.description = "The program %prog has the following commands"
         self.parser = CommandOptionParser(description=self.description)
 
 
+    def register_command_names(self, command_names):
+        for id in command_names:
+            self.command_ids.append(id)
+
     def execute(self, argv=None):
         argv = sys.argv if not argv else argv
 
+        #TODO: We might want to use default command!!
         if len(argv) == 1:
             return self.show_help()
+        # TODO: We have to actually use self.parser.parse_args()
+        print "system argvs {0}".format(sys.argv)
+        command, arguments = self.parse_argv(argv)
 
-        self.parse_argv(argv)
-
-        command = self.command
-        arguments = self.argv
         self.run_command(command, arguments)
 
     def run_command(self, cmd, argv):
@@ -50,17 +56,21 @@ class CommandController():
         except Exception, e:
             self.exception_handler(e)
 
-        command = Command(stdout=self.stdout, stderr=self.stderr, cmd=cmd)
+        command = Command(cmd, stdout=self.stdout, stderr=self.stderr)
         command.run_from_argv(argv)
 
     def parse_argv(self, argv):
-        self.argv = argv
+        # self.argv = argv
         # This should always be here
         self.prog = argv[0]
         #Expect subcommand else would have show_help
         self.command = argv[1]
         # We want to store the arguments or show help
         self.argv = argv[2:] or CommandController.DEFAULT_ARGUMENT
+        print "prog {0}".format(self.prog)
+        print "command {0}".format(self.command)
+        print "self argv {0}".format(self.argv)
+        return self.command, self.argv
 
     def get_command_class(self, cmd):
         try:
@@ -86,29 +96,47 @@ class CommandController():
         sys.exit(-1)
 
     def show_help(self, load_commands=True):
+        #let's load all registered commands and register them
+        for cmd in self.command_ids:
+            Command = self.get_command_class(cmd)
+            command = Command(cmd)
+            self.parser.add_subcommand(command)
 
         return self.stdout.write(self.parser.format_help())
 
 
-class BaseCommand():
-    help = ''
-    args = ''
+class BaseCommand(CommandMeta):
     option_list = (
-        make_option('--debug', action='store_true', dest='debug',
-                    default=False, help='Debug mode'),
-        make_option('--traceback', action='store_true', dest='traceback',
-                    default=True, help='Print traceback in error')
-    )
+            make_option('--debug',
+                        action='store_true',
+                        dest='debug',
+                        default=False,
+                        help='Debug mode'
+            ),
+            make_option('--traceback',
+                        action='store_true',
+                        dest='traceback',
+                        default=True,
+                        help='Print traceback in error'
+            )
+        )
 
     class Meta():
         help = 'something goes here'
         description = 'hola'
 
-    def __init__(self, stdout=None, stderr=None, cmd=None, parser=None):
-        self.cmd = cmd
-        self.meta = CommandMeta(cmd, parser, self.help)
+    def __init__(self, name, parser=None, help='', aliases=(), stdout=None, stderr=None):
+
         self.stdout = stdout or sys.stdout
         self.stderr = stderr or sys.stderr
+
+        CommandMeta.__init__(self, name, parser=parser, help=help, aliases=aliases)
+
+    def get_meta(self):
+        return
+
+    def get_version(self):
+        return get_version()
 
     def execute(self, *args, **options):
         try:
@@ -120,33 +148,10 @@ class BaseCommand():
                 sys.exit(1)
             else:
                 raise
-    def get_option_list(self):
-        return self.option_list
-
-    def get_parser(self, prog_name, subcommand):
-        print "Prog name {0}".format(prog_name)
-        parser = OptionParser(
-            prog=subcommand,
-            usage=self.get_usage(),
-            version=self.get_version(),
-            option_list=sorted(self.get_option_list())
-        )
-        parser.disable_interspersed_args()
-        return parser
-
-    def get_usage(self):
-        return "%prog command"
-
-    def get_version(self):
-        from scaffolder import get_version
-        return get_version()
-
-    def print_help(self, prog_name, subcommand):
-        parser = self.get_parser(prog_name, subcommand)
-        parser.print_help()
 
     def run_from_argv(self, argv):
-        parser = self.get_parser(sys.argv[0], sys.argv[1])
+        # parser = self.get_parser(sys.argv[0], sys.argv[1])
+        parser = self.parser
         options, args = parser.parse_args(argv)
         self.execute(*args, **options.__dict__)
         # self.execute(argv)
