02749f4b9da4b0abc2216b1ce0af4315c2cebf96
goliatone <burgosemi@gmail.com>
2014-02-02 22:38:22 -0500

    CLI: Refactor launch code, create CliApplication.

--- scaffolder/cli.py
+++ scaffolder/cli.py
@@ -4,12 +4,34 @@ import sys
 from scaffolder.core.commands import  CommandController
 
 
+COMMANDS = [
+    'template',
+    'vcs'
+]
+
+
+class CliApplication():
+    def __init__(self):
+        self.description
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        self._description = value
 
 def run(argv=None):
     if argv is None:
         argv = sys.argv
 
+    #Example of how we can access Meta class for each CMD
+    from scaffolder.commands.vcs import VcsCommand
+    meta = getattr(VcsCommand, 'Meta', None)
+
     manager = CommandController()
+    #make command ids available
+    manager.register_command_names(COMMANDS)
     manager.execute(argv)
     return 0
 
