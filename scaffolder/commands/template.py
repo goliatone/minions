88a87fad15692477e1c2471508d4694031b3b331
goliatone <burgosemi@gmail.com>
2014-02-02 22:37:53 -0500

    TemplateCommand, wip.

--- scaffolder/commands/template.py
+++ scaffolder/commands/template.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from optparse import make_option
+from optparse import OptionParser
+
 from scaffolder.core.commands import BaseCommand
 
 class TemplateCommand(BaseCommand):
-    pass
+    option_list = BaseCommand.option_list + (
+        make_option(
+            "-u",
+            "--url",
+            dest="url",
+            default='default',
+            help='Database router id.',
+            metavar="DATABASE"
+        ),
+    )
+
+    def __init__(self, name, parser=None, help='', aliases=(), stdout=None, stderr=None):
+        help = 'Template command help entry'
+        parser = OptionParser(
+            version=self.get_version(),
+            option_list=self.get_option_list(),
+            usage='\n  %prog {0} [OPTIONS] FILE...'.format(name)
+        )
+        aliases = ('tmp',)
+        BaseCommand.__init__(self, name, parser=parser, help=help, aliases=aliases)
+
+    def run(self, *args, **options):
+        url = options.get('url')
+        debug = options.get('debug')
+
+        print "Execute template {0}, {1}".format(url, debug)
+
