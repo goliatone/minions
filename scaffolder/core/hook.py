30eabd8cf9d917a771be6e03a018345e1666e8cb
goliatone <burgosemi@gmail.com>
2014-02-04 23:49:58 -0500

    Hook: Initial implementation of hooks.

--- scaffolder/core/hook.py
+++ scaffolder/core/hook.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+from subprocess import call, Popen
 
 
 class HookRunner():
 
     def run(self, path=None, cwd=None, hook='post'):
-        pass
+        script = os.path.join(path, 'hooks', hook)
+        if not os.path.isfile(script):
+            return
+        try:
+            print "Executing in context {0}".format(cwd)
+            Popen(script, cwd=cwd)
+
+        except Exception, e:
+            print e
