9085de60860ba33cd18e00864d49bf14e4cb495b
goliatone <burgosemi@gmail.com>
2014-02-06 22:12:39 -0500

    TemplateManager: Refactored metadata format. We now expect a yaml file.

--- scaffolder/core/template.py
+++ scaffolder/core/template.py
@@ -12,7 +12,7 @@ Can we read this?! That is a good question!
 """
 from textwrap import fill
 from clint.textui import puts, indent, colored
-from scaffolder.core.utils import clone_url, extract_directory
+from scaffolder.core.utils import clone_url, extract_directory, lower_keys
 from scaffolder.core.utils import Utils
 """
 TemplateManager class:
@@ -27,14 +27,13 @@ Methods:
 
 """
 class TemplateManager():
-    INIT_FILE = "__init__.*"
+    INIT_FILE = "meta.*"
 
     def __init__(self):
         self.metadata = {}
 
     def list(self, path='~/.cookiejar'):
         path = os.path.expanduser(path)
-        print path
         dirs = self.list_dirs(path)
         for dir in dirs:
             if os.path.isdir(dir):
@@ -45,10 +44,10 @@ class TemplateManager():
         print "Templates:\n"
         for name in self.metadata:
             info = self.metadata[name]
-            info.setdefault('Description', 'This template has no description')
+            info.setdefault('description', 'This template has no description')
             # " %-45s %-15s %15s" % (template, status, file_type)
-            title = "- {Template}:\n".format(**info)
-            desc = "{Description}".format(**info)
+            title = "- {template}:\n".format(**info)
+            desc = "{description}".format(**info)
             desc = fill(desc, width=70, initial_indent="  ", subsequent_indent="  ")
             puts(colored.green(
                 title+desc
@@ -58,16 +57,14 @@ class TemplateManager():
     def load_metadata(self, path):
         init_file = os.path.join(path, TemplateManager.INIT_FILE)
         template = os.path.basename(path)
-        print "Check dir: {}".format(template)
-        file = glob.glob(init_file)
-        if file:
-            content = open(file[0], 'r').readlines()
-            b = [i for i in range(len(content)) if content[i] == '"""\n']
-            if b.__len__() != 2:
-                return
-            content = "".join(content[b[0]+1:b[1]])
-            self.metadata[template] = yaml.load(content)
-    
+        file = glob.glob(init_file)[0]
+        try:
+            with open(file, 'r') as file:
+                metadata = yaml.load(file.read())
+                self.metadata[template] = lower_keys(metadata)
+        except:
+            pass
+
     def list_dirs(self, path):
         output = []
         for item in os.listdir(path):
@@ -87,7 +84,6 @@ class TemplateManager():
 
         src, dest = clone_url(src_path=src, tgt_path=dest)
         src, dest = extract_directory(src_path=src, tgt_path=dest)
-        print "SRC: {}\nTGT: {}".format(src, dest)
 
         call(["cp", "-R", src, dest])
 
