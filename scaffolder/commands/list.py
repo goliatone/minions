3dc9e45448211a5bd36c1f4e495eddef6e0e485e
goliatone <burgosemi@gmail.com>
2014-02-08 00:46:54 -0500

    ListCommand: Remove options, the command does not take options.

--- scaffolder/commands/list.py
+++ scaffolder/commands/list.py
@@ -6,32 +6,21 @@ from optparse import OptionParser
 from scaffolder.core.commands import BaseCommand
 from scaffolder.core.template import TemplateManager
 
-class TemplateCommand(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option(
-            "-u",
-            "--url",
-            dest="url",
-            default='default',
-            help='Database router id.',
-            metavar="DATABASE"
-        ),
-    )
+class ListCommand(BaseCommand):
 
     def __init__(self, name, help='', aliases=(), stdout=None, stderr=None):
         help = 'Template command help entry'
         parser = OptionParser(
             version=self.get_version(),
             option_list=self.get_option_list(),
-            usage='\n  %prog {0} [OPTIONS] FILE...'.format(name)
+            usage='\n  %prog {0} [OPTIONS]'.format(name)
         )
         aliases = ('tmp',)
         BaseCommand.__init__(self, name, parser=parser, help=help, aliases=aliases)
 
     def run(self, *args, **options):
-        url = options.get('url')
-        debug = options.get('debug')
         manger = TemplateManager()
         manger.list()
-        print "Execute template {0}, {1}".format(url, debug)
 
+    def get_default_option(self):
+        return []
